1b51fdfa82dd4b48188a14d1280102722f5e4b01
Adam Bretz <arbretz@gmail.com>
2014-09-19 16:08:55 -0400

    Cleanup for version 1.

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
-good-http
-=========
+# good-http
 
 Http(s) broadcasting for Good process monitor
+
+[![Build Status](https://travis-ci.org/hapijs/good-file.svg?branch=master)](https://travis-ci.org/hapijs/good-file) ![Current Version](https://img.shields.io/npm/v/good-http.svg)
+
+Lead Maintainer: [Adam Bretz](https://github.com/arb)
+
+## Usage
+
+`good-http` is a [good-reporter](https://github.com/hapijs/good-reporter) implementation to write [hapi](http://hapijs.com/) server events to remote endpoints. It makes a "POST" request with a JSON payload to the supplied `endpoint`.
+
+## Good Http
+### new GoodHttp ([options])
+
+creates a new GoodFile object with the following arguments
+- `[options]` - optional arguments object
+	- `[events]` - an object of key value paris. Defaults to `{ request: [], log: [] }`.
+		- `key` - one of ("request", "log", "error", or "ops") indicating the hapi event to subscribe to
+		- `value` - an array of tags to filter incoming events. An empty array indicates no filtering.
+	- `threshold` - number of events to hold before transmission. Defaults to `20`. Set to `0` to have every event start transmission instantly. It is strongly suggested to have a set threshold to make data transmission more efficient.
+	- `endpoint` - full path to remote server to transmit logs.
+	- `timeout` - `Wreck` timeout option. Defaults to `60000`.
+	- `headers` - additional headers to send during transmission. Defaults to `{}`. "content-type:application/json" is always used.
+
+### GoodHttp Methods
+`good-file` implements the [good-reporter](https://github.com/hapijs/good-reporter) interface as has no additional public methods.
+
+- `stop(callback)` - `GoodHttp` will make a final attempt to transmit anything remaining in it's internal event queue when `stop` is called.
---
+++ test/index.js
@@ -0,0 +1,301 @@
+// Load modules
+
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
+var GoodHttp = require('..');
+var Hapi = require('hapi');
+var Async = require('async');
+
+// Declare internals
+
+var internals = {};
+
+internals.isSorted = function (elements) {
+
+    var i = 0;
+    var il = elements.length;
+
+    while (i < il && elements[i+1]) {
+
+        if (elements[i].timestamp > elements[i+1].timestamp) {
+            return false;
+        }
+        ++i;
+    }
+    return true;
+};
+
+internals.makeServer = function (handler) {
+
+    var server = new Hapi.Server('127.0.0.1', 0);
+
+    server.route({
+        method: 'POST',
+        path: '/logs',
+        handler: handler
+    });
+
+    return server;
+};
+
+// Test shortcuts
+
+var describe = lab.describe;
+var it = lab.it;
+var before = lab.before;
+var after = lab.after;
+var expect = Lab.expect;
+
+describe('GoodHttp', function() {
+
+    it('throws an error without using new', function(done) {
+
+        expect(function () {
+
+            var reporter = GoodHttp({
+                endpoint: 'www.github.com'
+            });
+        }).to.throw('GoodHttp must be created with new');
+
+        done();
+    });
+
+    it('throws an error if missing endpoint', function (done) {
+
+        expect(function () {
+            var reporter = new GoodHttp({});
+        }).to.throw('endpoint must be specified');
+
+        done();
+    });
+
+    describe('#report', function () {
+
+        it('honors the threshold setting and sends the events in a batch', function (done) {
+
+            var hitCount = 0;
+            var server = internals.makeServer(function (request, reply) {
+
+                hitCount++;
+                var payload = request.payload;
+                var events = payload.events.log;
+
+                expect(request.headers['x-api-key']).to.equal('12345');
+                expect(payload.schema).to.equal('good-http');
+                expect(events.length).to.equal(5);
+
+                if (hitCount === 1) {
+                    expect(events[4].id).to.equal(4);
+                    expect(events[4].event).to.equal('log');
+                    reply();
+                }
+
+                if (hitCount === 2) {
+                    expect(events[4].id).to.equal(9);
+                    expect(events[4].event).to.equal('log');
+
+                    reply();
+                    done();
+                }
+            });
+
+            server.start(function () {
+
+                var reporter = new GoodHttp({
+                    endpoint: server.info.uri + '/logs',
+                    threshold: 5,
+                    headers: {
+                        'x-api-key': 12345
+                    },
+                    events: {
+                        log: []
+                    }
+                });
+
+                Async.eachSeries([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], function (item, next) {
+
+                    reporter.queue('log', {
+                        event: 'log',
+                        timestamp: Date.now(),
+                        id: item
+                    });
+                    reporter.report(function (error) {
+
+                        expect(error).to.not.exist;
+                        next();
+                    });
+                });
+            });
+        });
+
+        it('sends each event individually if threshold is 0', function (done) {
+
+            var hitCount = 0;
+            var server = internals.makeServer(function (request, reply) {
+
+                hitCount++;
+                var payload = request.payload;
+
+                expect(payload.events).to.exist;
+                expect(payload.events.log).to.exist;
+                expect(payload.events.log.length).to.equal(1);
+                expect(payload.events.log[0].id).to.equal(hitCount - 1);
+
+                if (hitCount === 10) {
+                    done();
+                }
+                reply();
+            });
+
+            server.start(function () {
+
+                var reporter = new GoodHttp({
+                    endpoint: server.info.uri + '/logs',
+                    threshold: 0,
+                    events: {
+                        log: []
+                    }
+                });
+
+                Async.eachSeries([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], function (item, next) {
+
+                    reporter.queue('log', {
+                        event: 'log',
+                        timestamp: Date.now(),
+                        id: item
+                    });
+                    reporter.report(function (error) {
+
+                        expect(error).to.not.exist;
+                        next();
+                    });
+                });
+            });
+
+        });
+
+        it('sends the events in an envelop grouped by type and ordered by timestamp', function(done) {
+
+            var hitCount = 0;
+            var server = internals.makeServer(function (request, reply) {
+
+                hitCount++;
+                var payload = request.payload;
+                var events = payload.events;
+
+                expect(request.headers['x-api-key']).to.equal('12345');
+                expect(payload.schema).to.equal('good-http');
+
+                payload = request.payload;
+                events = payload.events;
+
+                expect(events.log).to.exist;
+                expect(events.log.length).to.equal(2);
+
+                expect(events.request).to.exist;
+                expect(events.request.length).to.equal(3);
+
+
+                expect(internals.isSorted(events.log)).to.equal(true);
+                expect(internals.isSorted(events.request)).to.equal(true);
+
+                reply();
+
+                if (hitCount === 2) {
+                    done();
+                }
+            });
+
+            server.start(function () {
+
+                var reporter = new GoodHttp({
+                    endpoint: server.info.uri + '/logs',
+                    threshold: 5,
+                    headers: {
+                        'x-api-key': 12345
+                    },
+                    events: {
+                        log: [],
+                        request: []
+                    }
+                });
+
+                Async.eachSeries([1, 3, 5, 2, 4, 7, 9, 11, 6, 8], function (item, next) {
+
+                    var eventType = item % 2 === 0 ? 'log' : 'request';
+
+                    reporter.queue(eventType, {
+                        event: eventType,
+                        // Create a random timestamp to put them out of order
+                        timestamp: Math.floor(Date.now() + (Math.random() * 10000000000)),
+                        id: item
+                    });
+                    reporter.report(function (error) {
+
+                        expect(error).to.not.exist;
+                        next();
+                    });
+                });
+            });
+        });
+    });
+    describe('#stop', function () {
+
+        it('makes a last attempt to send any remaining log entries', function (done) {
+
+            var hitCount = 0;
+            var server = internals.makeServer(function (request, reply) {
+
+                hitCount++;
+                var payload = request.payload;
+                var events = payload.events;
+
+                payload = request.payload;
+                events = payload.events;
+
+                expect(events.log).to.exist;
+                expect(events.log.length).to.equal(2);
+
+                reply();
+            });
+
+            server.start(function () {
+
+                var reporter = new GoodHttp({
+                    endpoint: server.info.uri + '/logs',
+                    threshold: 3,
+                    headers: {
+                        'x-api-key': 12345
+                    },
+                    events: {
+                        log: []
+                    }
+                });
+
+                reporter.queue('log', {
+                    event: 'log',
+                    timestamp: Date.now(),
+                    id: 1
+                });
+                reporter.queue('log', {
+                    event: 'log',
+                    timestamp: Date.now(),
+                    id: 2
+                });
+
+                reporter.report(function (error) {
+
+                    expect(error).to.not.exist;
+
+                    reporter.stop(function (err) {
+
+                        expect(err).to.not.exist;
+                        expect(hitCount).to.equal(1);
+
+                        done();
+                    });
+                });
+            });
+        });
+    });
+});
